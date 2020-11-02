23919b91d31db89001452928600520344f8e9b71
Vlad <56343453+VladMikhalev1999@users.noreply.github.com>
2020-11-02 22:27:32 +0500

    AJAX
    Добавлен AJAX для таблицы FILMS (просмотр данных, добавление, редактирование, удаление)

---
+++ get_films.php
@@ -0,0 +1,27 @@
+<?php
+$db = mysqli_connect('localhost', 'root', '', 'stp');
+    mysqli_set_charset($db, 'utf8');
+    if (!$db) {
+        $e = mysqli_error();
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+    $src = null;
+    if (isset($_REQUEST["id"])) {
+        $id = $_REQUEST["id"]; 
+        if ($id == "1") {
+            $name = $_REQUEST["name"];
+            $src = mysqli_query($db, 'INSERT INTO STP_FILMS (NAME) VALUES (\'' . $name . '\')');
+        } else if ($id == "2") {
+           $name = $_REQUEST['name'];
+           $fid = $_REQUEST['fid'];
+           $src = mysqli_query($db, 'UPDATE STP_FILMS SET NAME = \'' . $name . '\' WHERE ID = ' . $fid);
+        } else if ($id == "3") {
+           $fid = $_REQUEST['fid'];
+           $src = mysqli_query($db, 'DELETE FROM STP_FILMS WHERE ID = ' . $fid);
+        }
+    }
+    $src = mysqli_query($db, 'SELECT * FROM STP_FILMS ORDER BY ID ASC');
+    $res = mysqli_fetch_all($src, MYSQLI_ASSOC);
+    echo json_encode($res);
+   
+?>
--- script.js
+++ script.js
@@ -15,12 +15,73 @@ function change_to_button(id, _hid, tid = null) {
 }
 
 function append_request(id, tid = null) {
-    if (tid != null) document.location.href = "index.php?id=" + id + "&name=" + document.getElementById(tid).value;
-    else document.location.href = "index.php?id=" + id; 
+    if (tid != null) return "get_films.php?id=" + id + "&name=" + document.getElementById(tid).value;
+    return document.location.href = "films.php?id=" + id; 
 }
 
 function rename(id, tid, sid) {
     let ind = document.getElementById(sid).selectedIndex;
-    if (tid != null) document.location.href = "index.php?id=" + id + "&name=" + document.getElementById(tid).value + "&fid=" + document.getElementById(sid).options[ind].value;
-    else document.location.href = "index.php?id=" + id + "&fid=" + document.getElementById(sid).options[ind].value;
+    if (tid != null) return "get_films.php?id=" + id + "&name=" + document.getElementById(tid).value + "&fid=" + document.getElementById(sid).options[ind].value;
+    return "get_films.php?id=" + id + "&fid=" + document.getElementById(sid).options[ind].value;
+}
+
+function ajax(id, tid = null, sid = null) {
+    var objXMLHttpRequest = new XMLHttpRequest();
+    objXMLHttpRequest.onreadystatechange = function() {
+    if(objXMLHttpRequest.readyState === 4) {
+        if(objXMLHttpRequest.status === 200) {
+            var table = document.getElementById("tbl_films");
+            update_table(table, objXMLHttpRequest.responseText)
+            update_lists(['sel', 'sel1'], objXMLHttpRequest.responseText)
+        } else {
+            alert('Error Code: ' +  objXMLHttpRequest.status);
+            alert('Error Message: ' + objXMLHttpRequest.statusText);
+        }
+    }
+    }
+    if (sid == null) objXMLHttpRequest.open('GET', append_request(id, tid));
+    else objXMLHttpRequest.open('GET', rename(id, tid, sid));
+    objXMLHttpRequest.send();
+}
+
+function update_lists(lids, response) {
+    let data = JSON.parse(response)
+    for (let i = 0; i < lids.length; i++) {
+        let lid = lids[i]
+        let sel = document.getElementById(lid)
+        while (sel.children.length != 0) {
+            sel.removeChild(sel.firstChild)
+        }
+        for (let j = 0; j < data.length; j++) {
+            let opt = document.createElement("option")
+            opt.innerText = data[j]["ID"]
+            sel.appendChild(opt)
+        }
+    }
+}
+
+function update_table(table, response) {
+    var x = document.createElement("tbody");
+    table.replaceChild(x, table.children[0]);
+    var body = table.children[0];
+    var data = JSON.parse(response)
+    var tr = document.createElement("tr");
+    var td = document.createElement("th");
+    td.innerText = "Номер"
+    tr.appendChild(td)
+    td = document.createElement("th");
+    td.innerText = "Название"
+    tr.appendChild(td)
+    body.appendChild(tr);
+    for (let i = 0; i < data.length; i++) {
+        let el = data[i]
+        tr = document.createElement("tr")
+        td = document.createElement("td")
+        td.innerText = el["ID"]
+        tr.appendChild(td);
+        td = document.createElement("td")
+        td.innerText = el["NAME"]
+        tr.appendChild(td)
+        body.appendChild(tr)
+    }
 }
